ed0400911e34cee64c661c6728644b03d888b9f6
leejooy96 <leejooy96@gmail.com>
2024-12-30 12:48:06 +0900

    docs: update example

--- example/qsu_example.dart
+++ example/qsu_example.dart
@@ -1,5 +1,12 @@
 import 'package:qsu/qsu.dart';
 
+/// For the various functions provided by the package, see the documentation: https://qsu.cdget.com/api
 void main() {
+  trim(' hello '); // Returns 'hello'
+
+  numberFormat(123456); // Returns '123,456'
+
   capitalizeFirst('abc'); // Returns 'Abc'
+
+  isEmail('abc@example.com'); // Returns true
 }
